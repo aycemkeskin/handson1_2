c67ca9196d60dda2f1d6aca11f38f51acfdb6642
aycemkeskin <keskinaycem@gmail.com>
2023-06-14 22:21:44 +0300

    test fixed

--- src/test/java/tr/edu/metu/sm703/HomeControllerTest.java
+++ src/test/java/tr/edu/metu/sm703/HomeControllerTest.java
@@ -38,6 +38,6 @@ public class HomeControllerTest {
         request.setPath("/");
         AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
         assertEquals(200, response.getStatusCode());
-        assertEquals("{\"message\":\"Hello World New\"}",  response.getBody());
+        assertEquals("{\"message sum of x, y, and z: \":10}",  response.getBody());
     }
 }
