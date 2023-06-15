02559760cb2f5f1f8e14886fc4bd204353c575ba
aycemkeskin <keskinaycem@gmail.com>
2023-06-15 23:21:38 +0300

    create failure in sast

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -1,8 +1,17 @@
 package tr.edu.metu.sm703;
 
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
@@ -15,6 +24,15 @@ public class HomeController {
 
     int sum = x + y + z;
     String password = "password123";
+
+    HttpRequest request;
+    String serializedObject = request.getParameters().toString();
+    ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(serializedObject)));
+    Object obj = in.readObject();
+
+    public HomeController() throws IOException, ClassNotFoundException {
+    }
+
     @Get
     public Map<String, Object> index() {
         return Collections.singletonMap("Total sum of x, y, and z: ", sum);
